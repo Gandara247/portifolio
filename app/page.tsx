17bdc8e099662cacf4d6ef62b17585c36d594004
Gandara <gustavogandarasilva@gmail.com>
2024-02-06 15:21:16 -0300

    feat: added projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import Encryption from "@/components/main/Encryption";
+import Footer from "@/components/main/Footer";
 import Hero from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
 import Image from "next/image";
 
@@ -8,6 +11,9 @@ export default function Home() {
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
+        <Encryption />
+        <Projects />
+        <Footer />
       </div>
     </main>
   );
